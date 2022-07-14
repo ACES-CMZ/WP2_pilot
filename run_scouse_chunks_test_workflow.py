daf563ab7654685e898ecb195ed1750e194ad3cb
jdhenshaw <jonathan.d.henshaw@gmail.com>
2022-07-14 13:17:17 +0200

    testing github workflow -- test data only -- to be removed

---
+++ run_scouse_chunks_test_workflow.py
@@ -0,0 +1,17 @@
+from scousepy import scouse
+
+# paths to important stuff - you shouldn't need to touch this
+datadir='./'
+outputdir='./test_workflow/'
+filename='HC3N_TP_7m_12m_feather' # note we remove the fits extension
+
+s1file='s1.0.scousepy' # You will need to update these accordingly!!
+s2file='s2.0.scousepy' # You will need to update these accordingly!!
+
+# Run first two stages - stage 1 should simply load up the scouse file
+# stage 2 will open the fitter.
+#
+# Once completed upload the decomposed stage 2 data to github and globus
+config_file=scouse.run_setup(filename, datadir, outputdir=outputdir)
+s = scouse.stage_1(config=config_file, interactive=False, s1file=s1file )
+s = scouse.stage_2(config=config_file, refit=False, s1file=s1file, s2file=s2file)
