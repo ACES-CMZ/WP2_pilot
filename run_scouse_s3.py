b5c5be973245431126beafc19e9ed87b41e18400
jdhenshaw <jonathan.d.henshaw@gmail.com>
2022-12-01 20:43:32 +0100

    s3 fitting

---
+++ run_scouse_s3.py
@@ -0,0 +1,19 @@
+from scousepy import scouse
+
+# paths to important stuff - you shouldn't need to touch this
+datadir='./'
+outputdir='./'
+filename='HC3N_TP_7m_12m_feather' # note we remove the fits extension
+
+s1file='s1.combine.scousepy' # You will need to update these accordingly!!
+s2file='s2.combine.scousepy' # You will need to update these accordingly!!
+tolver='v0'
+# Run first two stages - stage 1 should simply load up the scouse file
+# stage 2 will open the fitter.
+#
+# Once completed upload the decomposed stage 2 data to github and globus
+config_file=scouse.run_setup(filename, datadir, outputdir=outputdir)
+s = scouse.stage_1(config=config_file, interactive=False, s1file=s1file )
+s = scouse.stage_2(config=config_file, refit=False, s1file=s1file, s2file=s2file)
+print(s.lenspec)
+s = scouse.stage_3(config=config_file+str('.'+tolver), s3file='s3.scousepy.'+tolver)
